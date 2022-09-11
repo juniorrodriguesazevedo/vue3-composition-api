bd37d8b27cfe3e317acb4e7898f5d43edaf30ea1
Junior Rodrigues <spider_junior01@hotmail.com>
2022-09-11 00:08:46 -0300

    indentação

--- src/views/Movies/MovieIndex.vue
+++ src/views/Movies/MovieIndex.vue
@@ -1,19 +1,15 @@
 <template>
-    <div>
-        <h1>Lista de Filmes</h1>
-    </div>
+  <div>
+    <h1>Lista de Filmes</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'MovieIndex',
-    components: {
-    
-  }
-    
-}
+  name: "MovieIndex",
+  components: {},
+};
 </script>
 
 <style >
-    
 </style>
